ce20a1b391e2ae25df6b046445670508ed80d4f8
swd-d <swdchw@gmail.com>
2022-06-17 22:10:59 +0500

    config for v2

---
+++ v2/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: '', // Токен вашего бота
+    type: 'Тян', // kristina, old, main, java, vk-sily, ROBOBOT, HappyLeo, laxtrax, сапожник, 3333, underbrine, Ваня-Петух, Тян, index, Леонид, post, Двач-бот, lingvobot, Добрый, рифмыИпанчи, Рудской, Крабик, chat-bot-evie, тролль-бот, русский-чат-бот, бот-онлайн, чат-с-ботом, разговор-с-ботом, pbot, patrick, sponge-bob, Бэтмен, Ruzek, ОРКИ, VersusBattle, Владик, Соколов, Oddy, Nekropolisss, VirusMC, Нармина, mrazish, строитель, МХ-лавер, Cpiridon, ирма, инабаа, Slavon, DreamHack, m8l8th, animus, Bitard, Puge, GiN, чат-бот-киса, чат-с-девушкой, тролль-бот-обезьяна
+    channelID: '', // Айди канала, куда бот будет писать (при каждом сообщении туда, оставить пустым если такого не будет)
+    canBotWriteToOtherChannel: true, // Если указан channelID может ли бот писать куда-то кроме него? (при пинге)
+    // Статус
+    botonlineStatus: 'idle', // // Устанавливает статус бота: "online", "idle", "dnd", "invisible"
+    bottextStatus: 'за чатиком..', // // Текст, который будет отображен в статусе бота
+    typeofStatus: 3, // Тип статуса: 0 "Играет", 1 "Стримит" (только твич), 2 "Слушает", 3 "Смотрит", 5 "Соревнуется в"
+}
